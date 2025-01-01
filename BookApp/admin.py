fb5a2b81e6f1b031e9d7b261fdcce5f00a55f461
BOSTONE069 <bostoneochieng69@gmail.com>
2025-01-01 14:13:59 +0300

    Mapped the tables for books creted with the users in the models

--- BookApp/admin.py
+++ BookApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import BookModel
 # Register your models here.
+class BookModelAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'price')
+admin.site.register(BookModel, BookModelAdmin)
